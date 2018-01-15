630a26658d9cfb3426af535f4ad9f1690fafb071
HexelDev <hdhexel@gmail.com>
2018-01-15 23:23:18 +0100

    Basic authentication class

--- install/setup.sql
+++ install/setup.sql
@@ -5,8 +5,8 @@ CREATE TABLE articles(
     preview varchar(255) NOT NULL,
     month int(2),
     year int(4),
-    publish_date datetime NULL,
-    published int(1) DEFAULT 0,
+    publishing_date datetime NULL,
+    allowed int(1) DEFAULT 0,
     PRIMARY KEY (id)
 );
 
@@ -20,6 +20,8 @@ CREATE TABLE admin(
 
 CREATE TABLE admin_sessions(
     admin_id int(12) NOT NULL,
+    session_id int(12) NOT NULL,
+    start datetime NULL,
     ip varchar(16) NOT NULL,
     token varchar(255) NOT NULL
     PRIMARY KEY (id)
--- src/core/authentication.php
+++ src/core/authentication.php
@@ -4,19 +4,29 @@
  * //File description
  * License: BSD-2-Clause
  */
+namespace FastBlog\Core;
 
 use \ORM;
 
 class Authentication {
 
+    private $config;
     protected $authenticated = false;
 
-    public function __construct() {
-        if((isset($_SESSION['TIME']) && $_SESSION['TIME'] > time() - (7 * 24 * 60 * 60)) && isset($_COOKIE['fb_auth_tkn']) && ORM::forTable('admin_sessions')->where(array(
-            'ip' => $_SERVER['REMOTE_ADDR'],
-            'token' => $_COOKIE['fb_auth_tkn']
-            ))->findOne()) {
-                $this->authenticated = true;
+    public function __construct($config) {
+        $this->config = $config;
+        if(isset($_COOKIE['fb_auth_tkn'])) {
+            $session = ORM::forTable('admin_sessions')->where(array(
+                'session_id' => session_id(),
+                'token' => $_COOKIE['fb_auth_tkn']
+            ))->findOne();
+            if($session) {
+                if($session->get('ip') == $_SERVER['REMOTE_ADDR']) {
+                    $this->authenticated = true;
+                } else {
+                    $session->delete();
+                }
+            }
         }
     }
 
@@ -32,15 +42,29 @@ class Authentication {
         return false;
     }
 
-    public function authenticateUser($user_id) {
+    public function createSession($user_id, $rememberme) {
         $token = bin2hex(openssl_random_pseudo_bytes(16));
+        $now = date('Y-m-d H:i:s');
         ORM::forTable('admin_sessions')->create(array(
             'admin_id' => $user_id,
+            'session_id' => session_id(),
+            'start' => $now,
             'ip' => $_SERVER['REMOTE_ADDR'],
             'token' => $token
         ));
 
+        if($rememberme) {
+            setcookie('fb_auth_tkn', $token, time() + (86400 * 30),"/" + $this->config["paths"]["admin"], "", $this->config["https"]);
+        } else {
+            setcookie('fb_auth_tkn', $token, 0,"/" + $this->config["paths"]["admin"], "", $this->config["https"]);
+        }
+
         $_SESSION['TIME'] = time();
-        $_COOKIE['fb_auth_tkn'] = $token;
+    }
+
+    public function logout(){
+        ORM::forTable('admin_sessions')->where(array(
+            'session_id' => session_id()
+        ))->findOne()->delete();
     }
 }
