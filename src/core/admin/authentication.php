c70f7612bd2e1e81ab337ea4548d31082b660227
HexelDev <hdhexel@gmail.com>
2018-02-21 21:11:58 +0100

    Cookie deletion #4

--- src/core/admin/authentication.php
+++ src/core/admin/authentication.php
@@ -18,23 +18,37 @@ class ACPAuthentication {
         if(isset($_COOKIE['fb_auth_tkn'])) {
             $session = ORM::forTable('admin_sessions')->where(array(
                 'session_id' => session_id(),
-                'token' => $_COOKIE['fb_auth_tkn']
+                'token' => $_COOKIE['fb_auth_tkn'],
+                'ip' => $_SERVER['REMOTE_ADDR']
             ))->findOne();
             if($session) {
-                $interval = DateTime::createFromFormat('Y-m-d H:i:s',$session->get('start'))->diff(date('Y-m-d H:i:s'));
-                if($interval->d < 30 && $session->get('ip') == $_SERVER['REMOTE_ADDR']) {
+                $interval = DateTime::createFromFormat('Y-m-d H:i:s', $session->get('start'))->diff(new DateTime('Y-m-d H:i:s'));
+                if($interval->d < 30) {
                     $this->authenticated = true;
-                } else {
-                    $session->delete();
                 }
+            }
+
+            if(!$this->authenticated) {
+                $session->delete();
+                $this->unsetCookies();
             }
         }
     }
 
+    /**
+     * Return if an user is logged or not
+     * @return bool
+     */
     public function isAuthenticated() {
         return $this->authenticated;
     }
 
+    /**
+     * Verify the correction of an user password upon login
+     * @param $email
+     * @param $password
+     * @return bool
+     */
     public function comparePassword($email, $password) {
         $user = ORM::forTable('admin')->where(array('email' => $email))->findOne();
         if($user){
@@ -43,6 +57,24 @@ class ACPAuthentication {
         return false;
     }
 
+    /*
+     * The client side session is currently detected using:
+     *   - Token (stored in the 'fb_auth_tkn' cookie)
+     *   - IP (implicitly detected by fb)
+     *
+     * The session is stored using:
+     *   - Admin ID
+     *   - Session ID
+     *   - Starting date-time
+     *   - IP (using during logon)
+     *   - Token (the sme stored client side)
+     */
+
+    /**
+     * Create a session given the user id (and a boolean used to remember the session client-side)
+     * @param $user_id
+     * @param $rememberme
+     */
     public function createSession($user_id, $rememberme) {
         $token = bin2hex(openssl_random_pseudo_bytes(16));
         $now = date('Y-m-d H:i:s');
@@ -55,15 +87,27 @@ class ACPAuthentication {
         ));
 
         if($rememberme) {
-            setcookie('fb_auth_tkn', $token, time() + (86400 * 30),"/" + $this->config["paths"]["admin"], "", $this->config["https"]);
+            setcookie('fb_auth_tkn', $token, time() + (86400 * 30),"/".$this->config["paths"]["admin"], "", $this->config["https"]);
         } else {
-            setcookie('fb_auth_tkn', $token, 0,"/" + $this->config["paths"]["admin"], "", $this->config["https"]);
+            setcookie('fb_auth_tkn', $token, 0,"/".$this->config["paths"]["admin"], "", $this->config["https"]);
         }
     }
 
-    public function logout(){
+    /**
+     * Completely delete a session (db entry included)
+     */
+    public function destroySession(){
         ORM::forTable('admin_sessions')->where(array(
             'session_id' => session_id()
         ))->findOne()->delete();
+        $this->unsetCookies();
+    }
+
+    /**
+     * Unset 'session' and 'fb_auth_tkn' cookies
+     */
+    public function unsetCookies(){
+        unset($_COOKIE['fb_auth_tkn']);
+        session_destroy();
     }
 }
