f2bc04b44cc5055744f4136862ffdd74896a19de
HexelDev <hdhexel@gmail.com>
2018-02-24 22:11:41 +0100

    Removed 'admin_sessions' table

--- install/setup.sql
+++ install/setup.sql
@@ -6,7 +6,7 @@ CREATE TABLE articles(
     month int(2),
     year int(4),
     publishing_date datetime NULL,
-    allowed int(1) DEFAULT 0,
+    published int(1) DEFAULT 0,
     PRIMARY KEY (id)
 );
 
@@ -15,14 +15,5 @@ CREATE TABLE admin(
     nickname varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE admin_sessions(
-    admin_id int(12) NOT NULL,
-    session_id int(12) NOT NULL,
-    start datetime NULL,
-    ip varchar(16) NOT NULL,
-    token varchar(255) NOT NULL
     PRIMARY KEY (id)
 );
--- src/core/admin/authentication.php
+++ src/core/admin/authentication.php
@@ -16,21 +16,16 @@ class ACPAuthentication {
     public function __construct($config) {
         $this->config = $config;
         if(isset($_COOKIE['fb_auth_tkn'])) {
-            $session = ORM::forTable('admin_sessions')->where(array(
-                'session_id' => session_id(),
-                'token' => $_COOKIE['fb_auth_tkn'],
-                'ip' => $_SERVER['REMOTE_ADDR']
-            ))->findOne();
-            if($session) {
-                $interval = DateTime::createFromFormat('Y-m-d H:i:s', $session->get('start'))->diff(new DateTime('Y-m-d H:i:s'));
+            if(isset($_SESSION['token']) && $_SESSION['token'] === $_COOKIE['fb_auth_tkn']) {
+                $now = new DateTime();
+                $interval = DateTime::createFromFormat('Y-m-d H:i:s', $_SESSION['start'])->diff(DateTime::createFromFormat('Y-m-d H:i:s',$now->format('Y-m-d H:i:s')));
                 if($interval->d < 30) {
                     $this->authenticated = true;
+                } else {
+                    $this->destroySession();
                 }
-            }
-
-            if(!$this->authenticated) {
-                $session->delete();
-                $this->unsetCookies();
+            } else {
+                $this->destroySession();
             }
         }
     }
@@ -72,48 +67,41 @@ class ACPAuthentication {
 
     /**
      * Create a session given the user id (and a boolean used to remember the session client-side)
-     * @param $user_id
+     * @param $admin_id
      * @param $rememberme
      */
-    public function createSession($user_id, $rememberme) {
+    public function createSession($admin_id, $rememberme) {
         $token = bin2hex(openssl_random_pseudo_bytes(16));
         $now = date('Y-m-d H:i:s');
-        ORM::forTable('admin_sessions')->create(array(
-            'admin_id' => $user_id,
-            'session_id' => session_id(),
-            'start' => $now,
-            'ip' => $_SERVER['REMOTE_ADDR'],
-            'token' => $token
-        ));
 
-        if($rememberme) {
+        if($rememberme === "true") {
             setcookie('fb_auth_tkn', $token, time() + (86400 * 30),"/".$this->config["paths"]["admin"], "", $this->config["https"]);
         } else {
             setcookie('fb_auth_tkn', $token, 0,"/".$this->config["paths"]["admin"], "", $this->config["https"]);
         }
 
-        $user = ORM::forTable('admin')->findOne($user_id);
+        $user = ORM::forTable('admin')->findOne($admin_id);
 
-        $_SESSION['username'] = $user->get('username');
-        $_SESSION['email'] = $user->get('email');
+        $_SESSION['admin_id'] = $user->get('id');
+        $_SESSION['start'] = $now;
+        $_SESSION['token'] = $token;
+        $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+        $_SESSION['username'] = $user->get('nickname');
     }
 
     /**
      * Completely delete a session (db entry included)
      */
-    public function destroySession(){
-        ORM::forTable('admin_sessions')->where(array(
-            'session_id' => session_id()
-        ))->findOne()->delete();
-        $this->unsetCookies();
-        $this->destroy();
+    public function destroySession() {
+        session_destroy();
+        $this->unsetCookie();
     }
 
     /**
-     * Unset 'session' and 'fb_auth_tkn' cookies
+     * Unset 'fb_auth_tkn' cookies
      */
-    public function destroy(){
+    public function unsetCookie() {
+        setcookie ('fb_auth_tkn', "", time() - 3600);
         unset($_COOKIE['fb_auth_tkn']);
-        session_destroy();
     }
 }
