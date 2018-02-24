1d138fa1fd1cc7868266250822241263803aa5f1
HexelDev <hdhexel@gmail.com>
2018-02-24 22:30:19 +0100

    UCP/Users routes

--- src/routes/admin/ucp/routes.php
+++ src/routes/admin/ucp/routes.php
@@ -4,3 +4,22 @@
  * Routes file for the acp user panel
  * License: BSD-2-Clause
  */
+namespace FastBlog\Core;
+
+$klein->with('/'.$fastblog->config["paths"]["admin"].'/me', function () use($klein, $fastblog) {
+    $klein->respond(function ($request, $response, $service) use($fastblog) {
+        if($fastblog->authentication->isAuthenticated()) {
+            $service->render(APP_PATH.'views/admin/ucp/submenu.phtml',
+                array(
+                    "admin_path" => $fastblog->config["paths"]["admin"]
+                )
+            );
+        }
+    });
+
+    $klein->respond('', function ($request, $response, $service) use($fastblog) {
+        if($fastblog->authentication->isAuthenticated()) {
+            $service->render(APP_PATH.'views/admin/ucp/index.phtml');
+        }
+    });
+});
--- src/routes/admin/users/routes.php
+++ src/routes/admin/users/routes.php
@@ -4,3 +4,22 @@
  * Routes file for the acp users configuration
  * License: BSD-2-Clause
  */
+namespace FastBlog\Core;
+
+$klein->with('/'.$fastblog->config["paths"]["admin"].'/users', function () use($klein, $fastblog) {
+    $klein->respond(function ($request, $response, $service) use($fastblog) {
+        if($fastblog->authentication->isAuthenticated()) {
+            $service->render(APP_PATH.'views/admin/users/submenu.phtml',
+                array(
+                    "admin_path" => $fastblog->config["paths"]["admin"]
+                )
+            );
+        }
+    });
+
+    $klein->respond('', function ($request, $response, $service) use($fastblog) {
+        if($fastblog->authentication->isAuthenticated()) {
+            $service->render(APP_PATH.'views/admin/users/index.phtml');
+        }
+    });
+});
