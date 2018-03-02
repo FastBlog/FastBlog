95d0e622ff5e72b9efcea958c4157439d696f93d
HexelDev <hdhexel@gmail.com>
2018-03-02 12:15:13 +0100

    tmp articles list implementation

--- src/routes/admin/articles/functions/routes.php
+++ src/routes/admin/articles/functions/routes.php
@@ -6,6 +6,32 @@
  */
 namespace FastBlog\Core;
 
+use \ORM;
+
+$klein->with('/'.$fastblog->config["paths"]["admin"].'/articles/list', function () use($klein, $fastblog) {
+    $klein->respond('/', function ($request, $response, $service) use ($fastblog) {
+
+        $n = 10; //tmp value
+        $array = array();
+
+        $articles = ORM::forTable('articles')->orderByAsc('id')->findMany()->limit($n);
+
+        $i = 0;
+        foreach($articles as $article) {
+            $array[$i] = new Article($article->get('id'));
+            $i++;
+        }
+
+        if ($article) {
+            $service->render(APP_PATH . 'views/admin/articles/list/index.phtml', array(
+                "articles" => $array
+            ));
+        } else {
+            $service->redirect($fastblog->config["paths"]["admin"].'/articles', 302);
+        }
+    });
+});
+
 include SRC_PATH.'routes/admin/articles/functions/new/routes.php';
 
 include SRC_PATH.'routes/admin/articles/functions/edit/routes.php';
