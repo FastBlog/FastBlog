f60bbe456649e626f1a44c5ebe83004ef81b111d
HexelDev <hdhexel@gmail.com>
2018-03-19 15:53:00 +0100

    Basic edit and new pages

--- src/routes/admin/articles/new/routes.php
+++ src/routes/admin/articles/new/routes.php
@@ -8,13 +8,17 @@ namespace FastBlog\Core;
 
 $klein->with('/'.$fastblog->config["paths"]["admin"].'/articles/new', function () use($klein, $fastblog) {
     $klein->respond('GET', '', function ($request, $response, $service) use ($fastblog) {
-        $service->render(APP_PATH . 'views/admin/articles/new/index.phtml');
+        if ($fastblog->authentication->isAuthenticated()) {
+            $service->render(APP_PATH . 'views/admin/articles/new/index.phtml');
+        }
     });
 
     $klein->respond('POST', '/save', function ($request, $response, $service) use ($fastblog) {
-        if($fastblog->authentication->isAuthenticated()) {
+        if ($fastblog->authentication->isAuthenticated()) {
             $new = new ACPNewArticle($request->alias, $request->preview, $request->datetime, $request->published, $request->content);
-            $new->create();
+            $result = $new->create();
+
+            if(!$result) $response->code(406);
         }
     });
 });
